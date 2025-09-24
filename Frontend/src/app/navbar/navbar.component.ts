4cf1d7a9432f3d1b95a651800b9d9ee1326bbd7a
Al1nn <artistulambitios@gmail.com>
2025-09-24 14:15:27 +0300

    Safe build

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { environment } from './../environments/environment';
- import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+ import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { StoreService } from '../store/store.service';
 import { catchError, map } from 'rxjs';
 import { isPlatformBrowser } from '@angular/common';
 import { IToken, IUser } from '../models/IUser';
 import { Router } from '@angular/router';
-
+import { OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-navbar',
@@ -32,12 +32,10 @@ export class NavbarComponent implements OnInit{
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       this.store.authService.refreshToken().subscribe();
-
     }
     this.loggedIn$.pipe(
       map(loggedIn => {
         if(loggedIn){
-          console.log("User is logged in");
 
           this.token$.pipe(
             map ( (token: IToken | null) => {
@@ -70,15 +68,28 @@ export class NavbarComponent implements OnInit{
         this.store.authService.setLoggedIn(false);
         this.store.alertifyService.message('You have been logged out');
         this.router.navigate(['/']);
+        this.dropdownOpen = false;
       }
     });
   }
 
   toggleDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
-    console.log("Dropdown state:", this.dropdownOpen);
   }
 
+  @HostListener('document:click', ['$event'])
+    onDocumentClick(event: Event): void {
+      const target = event.target as HTMLElement;
+      const dropdownElement = document.getElementById('dropdownMenu');
+      const dropdownButton = event.target as HTMLElement;
 
+      // Check if click is outside dropdown and not on the dropdown button
+      if (this.dropdownOpen &&
+          dropdownElement &&
+          !dropdownElement.contains(target) &&
+          !dropdownButton.closest('button')?.contains(target)) {
+        this.dropdownOpen = false;
+      }
+    }
 
 }
